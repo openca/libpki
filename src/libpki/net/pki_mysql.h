675a4b3cf5db878baede8b116a393e64ec96c180
Martin Hecht <mrbaseman@gmx.de>
2019-04-11 13:59:18 +0200

    make url a const in mysql interface

--- src/libpki/net/pki_mysql.h
+++ src/libpki/net/pki_mysql.h
@@ -7,18 +7,18 @@
 
 #include <mysql.h>
 
-MYSQL *db_connect ( URL *url );
+MYSQL *db_connect ( const URL *url );
 int db_close ( MYSQL *sql );
 
 #endif /* HAVE_MYSQL */
 
-char *parse_url_table ( URL * url );
-char *parse_url_dbname ( URL *url );
+char *parse_url_table ( const URL * url );
+char *parse_url_dbname ( const URL *url );
 
-PKI_MEM_STACK *URL_get_data_mysql ( char *url_s, ssize_t size );
-PKI_MEM_STACK *URL_get_data_mysql_url ( URL *url, ssize_t size );
+PKI_MEM_STACK *URL_get_data_mysql ( const char *url_s, ssize_t size );
+PKI_MEM_STACK *URL_get_data_mysql_url ( const URL *url, ssize_t size );
 
-int URL_put_data_mysql ( char *url_s, PKI_MEM *data );
-int URL_put_data_mysql_url ( URL *url, PKI_MEM *data );
+int URL_put_data_mysql ( const char *url_s, const PKI_MEM *data );
+int URL_put_data_mysql_url ( const URL *url, const PKI_MEM *data );
 
 #endif /* _LIBPKI_URL_MYSQL_H */
