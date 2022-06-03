a8099c50beb981907b3246140420bcaec9439907
Massimiliano Pala <m.pala@cablelabs.com>
2022-06-03 09:34:46 -0600

    Small fix for older application that use -1 as PKI_DATA_FORMAT instead of PKI_DATA_FORMAT_UNKNOWN when loading X509 objects without specifying the type.

--- src/pki_x509_mem.c
+++ src/pki_x509_mem.c
@@ -240,9 +240,21 @@ PKI_X509_STACK *PKI_X509_STACK_get_mem ( PKI_MEM *mem, PKI_DATATYPE type,
 	//       unknown datatype (PKI_DATATYPE_UNKNOW)
 	for (i = PKI_DATA_FORMAT_START; i < PKI_DATA_FORMAT_END; i++) {
 
+			// Fix for older applications using -1 as format
+		if (format == 4294967295) {
+			PKI_log_err("Wrong DATA format used in application (-1),"
+				"please replace with PKI_DATA_FORMAT_UNKNOWN (%d)", 
+						PKI_DATA_FORMAT_UNKNOWN);
+		}
+
 		// A Format was selected, so we skip the others
-		if (format != PKI_DATA_FORMAT_UNKNOWN && format != i)
-			continue;
+		if (format != 4294967295 && 
+		    format != PKI_DATA_FORMAT_UNKNOWN && 
+			format != i) {
+			
+			// We cannot process the type, skipping.
+		    continue;
+		}
 
 		if ((x_obj->value = __get_data_callback(mem, cb, i, cred)) != NULL) {
 
