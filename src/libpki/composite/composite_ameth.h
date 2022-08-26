4b56b2af25ae469c5b212142369f8a118742b62e
Massimiliano Pala <m.pala@cablelabs.com>
2022-08-26 16:01:28 -0600

    Intermediate Save: porting composite support within libpki.

---
+++ src/libpki/composite/composite_ameth.h
@@ -0,0 +1,258 @@
+/* BEGIN: composite_ameth.h */
+
+// Composite Crypto authentication methods.
+// (c) 2021 by Massimiliano Pala
+
+#include <stdio.h>
+// #include "internal/cryptlib.h"
+#include <openssl/x509.h>
+
+#include <libpki/composite/composite_internals.h>
+
+#include <openssl/evp.h>
+#include <openssl/asn1.h>
+
+// #include "crypto/asn1.h"
+// #include "crypto/evp.h"
+
+#ifndef OPENSSL_COMPOSITE_ASN1_METH_H
+#define OPENSSL_COMPOSITE_ASN1_METH_H
+
+#ifdef  __cplusplus
+extern "C" {
+#endif
+
+// ===============
+// Data Structures
+// ===============
+
+// Uses the Definition of NID_compositeCrypto and
+// NID_combinedCrypto
+
+// ======================
+// MACRO & Other Oddities
+// ======================
+
+// ===============================
+// EVP_PKEY_ASN1_METHOD Prototypes
+// ===============================
+
+// Implemented
+static int pub_decode(EVP_PKEY *pk, X509_PUBKEY *pub);
+
+// Implemented
+static int pub_encode(X509_PUBKEY *pub, const EVP_PKEY *pk);
+
+// Implemented
+static int pub_cmp(const EVP_PKEY *a, const EVP_PKEY *b);
+
+// Implemented
+static int pub_print(BIO *out, const EVP_PKEY *pkey, int indent, ASN1_PCTX *pctx);
+
+// Implemented
+static int priv_decode(EVP_PKEY *pk, const PKCS8_PRIV_KEY_INFO *p8inf);
+
+// Implemented
+static int priv_encode(PKCS8_PRIV_KEY_INFO *p8, const EVP_PKEY *pk);
+
+// Implemented
+static int priv_print(BIO *out, const EVP_PKEY *pkey, int indent, ASN1_PCTX *pctx);
+
+// Implemented
+static int pkey_size(const EVP_PKEY *pk);
+
+// Implemented
+static int pkey_bits(const EVP_PKEY *pk);
+
+// Implemented
+static int pkey_security_bits(const EVP_PKEY *pk);
+
+// Not Implemented
+static int param_decode(EVP_PKEY *pkey, const unsigned char **pder, int derlen);
+
+// Not Implemented
+static int param_encode(const EVP_PKEY *pkey, unsigned char **pder);
+
+// Not Implemented
+static int param_missing(const EVP_PKEY *pk);
+
+// Not Implemented
+static int param_copy(EVP_PKEY *to, const EVP_PKEY *from);
+
+// Implemented
+static int param_cmp(const EVP_PKEY *a, const EVP_PKEY *b);
+
+// Not Implemented
+static int param_print(BIO *out, const EVP_PKEY *pkey, int indent, ASN1_PCTX *pctx);
+
+// Not Implemented
+static int sig_print(BIO *out, const X509_ALGOR *sigalg, const ASN1_STRING *sig, int indent, ASN1_PCTX *pctx);
+
+// Implemented
+static void pkey_free(EVP_PKEY *pkey);
+
+// Implemented
+static int pkey_ctrl(EVP_PKEY *pkey, int op, long arg1, void *arg2);
+
+// ============================
+// Legacy Functions for old PEM
+// ============================
+
+// Not Implemented
+static int old_priv_decode(EVP_PKEY *pkey, const unsigned char **pder, int derlen);
+
+// Not Implemented
+static int old_priv_encode(const EVP_PKEY *pkey, unsigned char **pder);
+
+// ====================================
+// Custom ASN1 signature & verification
+// ====================================
+
+// Implemented
+static int item_verify(EVP_MD_CTX *ctx, const ASN1_ITEM *it, void *asn, X509_ALGOR *a, ASN1_BIT_STRING *sig, EVP_PKEY *pkey);
+
+// Implemented
+static int item_sign(EVP_MD_CTX *ctx, const ASN1_ITEM *it, void *asn, X509_ALGOR *alg1, X509_ALGOR *alg2, ASN1_BIT_STRING *sig);
+
+// Implemented
+static int siginf_set(X509_SIG_INFO *siginf, const X509_ALGOR *alg, const ASN1_STRING *sig);
+
+// ===================
+// EVP Check Functions
+// ===================
+
+// Not Implemented
+static int pkey_check(const EVP_PKEY *pkey);
+
+// Not Implemented
+static int pkey_public_check(const EVP_PKEY *pkey);
+
+// Not Implemented
+static int pkey_param_check(const EVP_PKEY *pkey);
+
+// =================================
+// Get/Set For Raw priv/pub key data
+// =================================
+
+// Not Implemented
+static int set_priv_key(EVP_PKEY *pk, const unsigned char *priv, size_t len);
+
+// Not Implemented
+static int set_pub_key(EVP_PKEY *pk, const unsigned char *pub, size_t len);
+
+// Not Implemented
+static int get_priv_key(const EVP_PKEY *pk, unsigned char *priv, size_t *len);
+
+// Not Implemented
+static int get_pub_key(const EVP_PKEY *pk, unsigned char *pub, size_t *len);
+
+// ===========================
+// ASN1 PKEY Method Definition
+// ===========================
+
+struct evp_pkey_asn1_method_st {
+    int pkey_id;
+    int pkey_base_id;
+    unsigned long pkey_flags;
+    char *pem_str;
+    char *info;
+    int (*pub_decode) (EVP_PKEY *pk, X509_PUBKEY *pub);
+    int (*pub_encode) (X509_PUBKEY *pub, const EVP_PKEY *pk);
+    int (*pub_cmp) (const EVP_PKEY *a, const EVP_PKEY *b);
+    int (*pub_print) (BIO *out, const EVP_PKEY *pkey, int indent,
+                      ASN1_PCTX *pctx);
+    int (*priv_decode) (EVP_PKEY *pk, const PKCS8_PRIV_KEY_INFO *p8inf);
+    int (*priv_encode) (PKCS8_PRIV_KEY_INFO *p8, const EVP_PKEY *pk);
+    int (*priv_print) (BIO *out, const EVP_PKEY *pkey, int indent,
+                       ASN1_PCTX *pctx);
+    int (*pkey_size) (const EVP_PKEY *pk);
+    int (*pkey_bits) (const EVP_PKEY *pk);
+    int (*pkey_security_bits) (const EVP_PKEY *pk);
+    int (*param_decode) (EVP_PKEY *pkey,
+                         const unsigned char **pder, int derlen);
+    int (*param_encode) (const EVP_PKEY *pkey, unsigned char **pder);
+    int (*param_missing) (const EVP_PKEY *pk);
+    int (*param_copy) (EVP_PKEY *to, const EVP_PKEY *from);
+    int (*param_cmp) (const EVP_PKEY *a, const EVP_PKEY *b);
+    int (*param_print) (BIO *out, const EVP_PKEY *pkey, int indent,
+                        ASN1_PCTX *pctx);
+    int (*sig_print) (BIO *out,
+                      const X509_ALGOR *sigalg, const ASN1_STRING *sig,
+                      int indent, ASN1_PCTX *pctx);
+    void (*pkey_free) (EVP_PKEY *pkey);
+    int (*pkey_ctrl) (EVP_PKEY *pkey, int op, long arg1, void *arg2);
+    /* Legacy functions for old PEM */
+    int (*old_priv_decode) (EVP_PKEY *pkey,
+                            const unsigned char **pder, int derlen);
+    int (*old_priv_encode) (const EVP_PKEY *pkey, unsigned char **pder);
+    /* Custom ASN1 signature verification */
+    int (*item_verify) (EVP_MD_CTX *ctx, const ASN1_ITEM *it, void *asn,
+                        X509_ALGOR *a, ASN1_BIT_STRING *sig, EVP_PKEY *pkey);
+    int (*item_sign) (EVP_MD_CTX *ctx, const ASN1_ITEM *it, void *asn,
+                      X509_ALGOR *alg1, X509_ALGOR *alg2,
+                      ASN1_BIT_STRING *sig);
+    int (*siginf_set) (X509_SIG_INFO *siginf, const X509_ALGOR *alg,
+                       const ASN1_STRING *sig);
+    /* Check */
+    int (*pkey_check) (const EVP_PKEY *pk);
+    int (*pkey_public_check) (const EVP_PKEY *pk);
+    int (*pkey_param_check) (const EVP_PKEY *pk);
+    /* Get/set raw private/public key data */
+    int (*set_priv_key) (EVP_PKEY *pk, const unsigned char *priv, size_t len);
+    int (*set_pub_key) (EVP_PKEY *pk, const unsigned char *pub, size_t len);
+    int (*get_priv_key) (const EVP_PKEY *pk, unsigned char *priv, size_t *len);
+    int (*get_pub_key) (const EVP_PKEY *pk, unsigned char *pub, size_t *len);
+} /* EVP_PKEY_ASN1_METHOD */ ;
+
+// The Definition of the EVP_PKEY_ASN1_METHOD is provided above and
+// it is taken from OPENSSL_SRC/include/crypto/asn1.h, see the
+// OPENSSL_SRC/include/ossl_typ.h
+const EVP_PKEY_ASN1_METHOD composite_asn1_meth = {
+    EVP_PKEY_COMPOSITE,       // int pkey_id;
+    EVP_PKEY_COMPOSITE,       // int pkey_base_id;
+    ASN1_PKEY_SIGPARAM_NULL,  // unsigned long pkey_flags; // ASN1_PKEY_SIGPARAM_NULL
+    "COMPOSITE",              // char *pem_str;
+    "Composite Crypto With Combined Keys", // char *info;
+    pub_decode,               // int (*pub_decode) (EVP_PKEY *pk, X509_PUBKEY *pub);
+    pub_encode,               // int (*pub_encode) (X509_PUBKEY *pub, const EVP_PKEY *pk);
+    pub_cmp,                  // int (*pub_cmp) (const EVP_PKEY *a, const EVP_PKEY *b);
+    pub_print,                // int (*pub_print) (BIO *out, const EVP_PKEY *pkey, int indent, ASN1_PCTX *pctx);
+    priv_decode,              // int (*priv_decode) (EVP_PKEY *pk, const PKCS8_PRIV_KEY_INFO *p8inf);
+    priv_encode,              // int (*priv_encode) (PKCS8_PRIV_KEY_INFO *p8, const EVP_PKEY *pk);
+    priv_print,               // int (*priv_print) (BIO *out, const EVP_PKEY *pkey, int indent, ASN1_PCTX *pctx);
+    pkey_size,                // int (*pkey_size) (const EVP_PKEY *pk);
+    pkey_bits,                // int (*pkey_bits) (const EVP_PKEY *pk);
+    pkey_security_bits,       // int (*pkey_security_bits) (const EVP_PKEY *pk);
+    param_decode,             // int (*param_decode) (EVP_PKEY *pkey, const unsigned char **pder, int derlen);
+    param_encode,             // int (*param_encode) (const EVP_PKEY *pkey, unsigned char **pder);
+    param_missing,            // int (*param_missing) (const EVP_PKEY *pk);
+    param_copy,               // int (*param_copy) (EVP_PKEY *to, const EVP_PKEY *from);
+    param_cmp,                // int (*param_cmp) (const EVP_PKEY *a, const EVP_PKEY *b);
+    param_print,              // int (*param_print) (BIO *out, const EVP_PKEY *pkey, int indent, ASN1_PCTX *pctx);
+    sig_print,                // int (*sig_print) (BIO *out, const X509_ALGOR *sigalg, const ASN1_STRING *sig, int indent, ASN1_PCTX *pctx);
+    pkey_free,                // void (*pkey_free) (EVP_PKEY *pkey);
+    pkey_ctrl,                // int (*pkey_ctrl) (EVP_PKEY *pkey, int op, long arg1, void *arg2);
+    // Legacy Functions for old PEM
+    old_priv_decode,          // int (*old_priv_decode) (EVP_PKEY *pkey, const unsigned char **pder, int derlen);
+    old_priv_encode,          // int (*old_priv_encode) (const EVP_PKEY *pkey, unsigned char **pder);
+    // Custom ASN1 signature verification
+    item_verify,              // int (*item_verify) (EVP_MD_CTX *ctx, const ASN1_ITEM *it, void *asn, X509_ALGOR *a, ASN1_BIT_STRING *sig, EVP_PKEY *pkey);
+    item_sign,                // int (*item_sign) (EVP_MD_CTX *ctx, const ASN1_ITEM *it, void *asn, X509_ALGOR *alg1, X509_ALGOR *alg2, ASN1_BIT_STRING *sig);
+    siginf_set,               // int (*siginf_set) (X509_SIG_INFO *siginf, const X509_ALGOR *alg, const ASN1_STRING *sig);
+    // PKEY checking interface
+    pkey_check,               // int (*pkey_check) (EVP_PKEY *pkey);
+    pkey_public_check,        // int (*pkey_public_check) (EVP_PKEY *pkey);
+    pkey_param_check,         // int (*pkey_param_check) (EVP_PKEY *pkey);
+    set_priv_key,             // int (*set_priv_key) (EVP_PKEY *pk, const unsigned char *priv, size_t len);
+    set_pub_key,              // int (*set_pub_key) (EVP_PKEY *pk, const unsigned char *pub, size_t len);
+    get_priv_key,             // int (*get_priv_key) (const EVP_PKEY *pk, unsigned char *priv, size_t *len);
+    get_pub_key,              // int (*get_pub_key) (const EVP_PKEY *pk, unsigned char *pub, size_t *len);
+};
+
+#ifdef  __cplusplus
+}
+#endif
+
+#endif // OPENSSL_COMPOSITE_AMETH_H
+
+/* END: composite_amenth.h */
---
+++ src/libpki/composite/composite_internals.h
@@ -0,0 +1,238 @@
+/* BEGIN: composite_local.h */
+
+// Composite Crypto authentication methods.
+// (c) 2021 by Massimiliano Pala
+
+#include <openssl/x509.h>
+// #include <crypto/evp/evp_local.h>
+#include <openssl/asn1t.h>
+
+#ifndef OPENSSL_COMPOSITE_LOCAL_H
+#define OPENSSL_COMPOSITE_LOCAL_H
+
+#ifdef  __cplusplus
+extern "C" {
+#endif
+
+// ========================
+// Composite Crypto Support
+// ========================
+
+// TODO: Remove the use of this hack that
+//       is meant to patch things until we
+//       can use the dynamic implementation
+//       of the ameth/pmeth
+#define NID_composite           1321
+#define NID_combined            1322
+
+// We need to find a solution for replacing
+// the use of NID_composite with the dynamic
+// version of it
+# define EVP_PKEY_COMPOSITE NID_composite
+# define EVP_PKEY_COMBINED NID_combined
+
+// Basic CTRL values for COMPOSITE support
+# define EVP_PKEY_CTRL_COMPOSITE_PUSH    0x201
+# define EVP_PKEY_CTRL_COMPOSITE_POP     0x202
+# define EVP_PKEY_CTRL_COMPOSITE_ADD     0x203
+# define EVP_PKEY_CTRL_COMPOSITE_DEL     0x204
+# define EVP_PKEY_CTRL_COMPOSITE_CLEAR   0x205
+
+// ==============================
+// Declarations & Data Structures
+// ==============================
+
+DEFINE_STACK_OF(EVP_PKEY);
+  // Provides the Definition for the stack of keys
+
+// DEFINE_STACK_OF_CONST(EVP_PKEY);
+  // Provides the Definition for the stack of keys
+
+typedef STACK_OF(EVP_PKEY) COMPOSITE_KEY;
+  // The Composite Key is just a Stack of EVP_PKEY
+
+DEFINE_STACK_OF(EVP_PKEY_CTX)
+  // Provides the definition for the stack of CTX
+
+DEFINE_STACK_OF(EVP_MD_CTX);
+  // Provides the Definition for the stack of keys
+
+// DEFINE_STACK_OF_CONST(EVP_PKEY_CTX)
+  // Provides the definition for the stack of CTX
+
+typedef struct {
+
+    EVP_PKEY_CTX * pkey_ctx;
+      // Stack of context for the components
+
+    EVP_MD_CTX * md_ctx;
+      // Stack of MD CTX for the components
+
+} COMPOSITE_CTX_ITEM;
+
+DEFINE_STACK_OF(COMPOSITE_CTX_ITEM);
+//DEFINE_STACK_OF_CONST(COMPOSITE_CTX_ITEM);
+
+typedef STACK_OF(COMPOSITE_CTX_ITEM) COMPOSITE_CTX;
+
+// Used to Concatenate the encodings of the different
+// components when encoding via the ASN1 meth (priv_encode)
+
+DEFINE_STACK_OF(ASN1_OCTET_STRING)
+
+// ====================
+// Functions Prototypes
+// ====================
+
+// COMPOSITE_KEY: Stack Aliases
+// ----------------------------
+
+#define COMPOSITE_KEY_new()                sk_EVP_PKEY_new_null()
+  // Allocates a new stack of EVP_PKEY
+
+#define COMPOSITE_KEY_new_null()           sk_EVP_PKEY_new_null()
+  // Allocates a new stack of EVP_PKEY
+
+#define COMPOSITE_KEY_push(key, val)       sk_EVP_PKEY_push(key, val)
+  // Pushes a new EVP_PKEY to the key
+
+#define COMPOSITE_KEY_pop(key)             sk_EVP_PKEY_pop(key)
+  // Removes the last EVP_PKEY from the key
+
+#define COMPOSITE_KEY_pop_free(key)        sk_EVP_PKEY_pop_free(key, EVP_PKEY_free)
+  // Removes the last EVP_PKEY from the key and frees memory
+
+#define COMPOSITE_KEY_num(key)             sk_EVP_PKEY_num(key)
+  // Gets the number of components of a key
+
+#define COMPOSITE_KEY_value(key, num)      sk_EVP_PKEY_value(key, num)
+  // Returns the num-th EVP_PKEY in the stack
+
+#define COMPOSITE_KEY_add(key, value, num) sk_EVP_PKEY_insert(key, value, num)
+  // Adds a component at num-th position
+
+#define COMPOSITE_KEY_del(key, num)        EVP_PKEY_free(sk_EVP_PKEY_delete(key, num))
+  // Deletesthe num-th component from the key
+
+#define COMPOSITE_KEY_get0(key, num)       sk_EVP_PKEY_value(key, num)
+  // Alias for the COMPOSITE_KEY_num() define
+
+#define COMPOSITE_KEY_dup(key)             sk_EVP_PKEY_deep_copy(key, EVP_PKEY_dup, EVP_PKEY_free)
+  // Duplicates (deep copy) the key
+
+// Free all components of the key
+static inline void COMPOSITE_KEY_clear(COMPOSITE_KEY *key) {
+
+  if (!key) return;
+
+  while (sk_EVP_PKEY_num(key) > 0) { 
+    sk_EVP_PKEY_pop_free(key, EVP_PKEY_free); 
+  }
+}
+
+static inline void COMPOSITE_KEY_free(COMPOSITE_KEY * key) {
+  
+  if (!key) return;
+
+  COMPOSITE_KEY_clear(key);
+  OPENSSL_free(key);
+}
+
+// COMPOSITE_CTX_ITEM: Prototypes
+// ------------------------------
+
+COMPOSITE_CTX_ITEM * COMPOSITE_CTX_ITEM_new_null();
+  // Allocates a new internal CTX item
+
+void COMPOSITE_CTX_ITEM_free(COMPOSITE_CTX_ITEM * it);
+  // Frees the memory associated with a CTX
+
+// COMPOSITE_CTX: Stack Aliases
+// ----------------------------
+
+#define COMPOSITE_CTX_new()                sk_COMPOSITE_CTX_ITEM_new_null()
+  // Allocates a new stack of EVP_PKEY
+
+#define COMPOSITE_CTX_new_null()           sk_COMPOSITE_CTX_ITEM_new_null()
+  // Allocates a new stack of EVP_PKEY
+
+#define COMPOSITE_CTX_push_item(ctx, val)  sk_COMPOSITE_CTX_ITEM_push(ctx, val)
+  // Pushes a new EVP_PKEY_CTX to the CTX
+
+#define COMPOSITE_CTX_pop_item(ctx)        sk_COMPOSITE_CTX_ITEM_pop(ctx)
+  // Removes the last EVP_PKEY_CTX from the CTX
+
+#define COMPOSITE_CTX_pop_free(ctx)        sk_COMPOSITE_CTX_ITEM_pop_free(ctx, COMPOSITE_CTX_ITEM_free)
+  // Removes the last EVP_PKEY_CTX from the CTX and frees memory
+
+#define COMPOSITE_CTX_num(ctx)             sk_COMPOSITE_CTX_ITEM_num(ctx)
+  // Gets the number of components of a ctx
+
+#define COMPOSITE_CTX_value(ctx, num)      sk_COMPOSITE_CTX_ITEM_value(ctx, num)
+  // Returns the num-th EVP_PKEY in the stack
+
+#define COMPOSITE_CTX_add_item(ctx, value, num) sk_COMPOSITE_CTX_ITEM_insert(ctx, value, num)
+  // Adds a component at num-th position
+
+#define COMPOSITE_CTX_del(ctx, num)        COMPOSITE_CTX_ITEM_free(sk_COMPOSITE_CTX_ITEM_delete(ctx, num))
+  // Deletesthe num-th component from the key
+
+#define COMPOSITE_CTX_get_item(ctx, num)   sk_COMPOSITE_CTX_ITEM_value(ctx, num)
+  // Alias for the COMPOSITE_KEY_num() define
+
+#define COMPOSITE_CTX_dup(ctx)             sk_COMPOSITE_CTX_ITEM_copy(ctx, COMPOSITE_CTX_ITEM_dup, COMPOSITE_CTX_ITEM_free)
+  // Duplicates (deep copy) the key
+
+int COMPOSITE_CTX_add(COMPOSITE_CTX * comp_ctx,
+                      EVP_PKEY_CTX  * pkey_ctx, 
+                      EVP_MD_CTX    * md_ctx,
+                      int             index);
+
+int COMPOSITE_CTX_add_pkey(COMPOSITE_CTX * comp_ctx,
+                           EVP_PKEY      * pkey,
+                           int             index);
+
+int COMPOSITE_CTX_push(COMPOSITE_CTX * comp_ctx,
+                      EVP_PKEY_CTX  * pkey_ctx,
+                      EVP_MD_CTX    * md_ctx);
+
+int COMPOSITE_CTX_push_pkey(COMPOSITE_CTX * comp_ctx,
+                            EVP_PKEY      * pkey);
+
+int COMPOSITE_CTX_get0(COMPOSITE_CTX  * comp_ctx,
+                       int              index,
+                       EVP_PKEY_CTX  ** pkey_ctx,
+                       EVP_MD_CTX    ** md_ctx);
+
+int COMPOSITE_CTX_pkey_get0(COMPOSITE_CTX  * comp_ctx,
+                            EVP_PKEY      ** pkey_ctx,
+                            int              index);
+
+int COMPOSITE_CTX_pop(COMPOSITE_CTX * comp_ctx,
+                      EVP_PKEY_CTX  ** pkey_ctx,
+                      EVP_MD_CTX    ** md_ctx);
+
+void COMPOSITE_CTX_clear(COMPOSITE_CTX *ctx);
+
+void COMPOSITE_CTX_free(COMPOSITE_CTX * ctx);
+
+// Returns the total size of the components
+int COMPOSITE_KEY_size(COMPOSITE_KEY * key);
+
+// Returns the total size in bits of the components
+// (does this even make sense ?)
+int COMPOSITE_KEY_bits(COMPOSITE_KEY * bits);
+
+// Returns the security bits of the composite key
+// which is the lowest (if the OR logic is implemented)
+// or is the highest (if the AND logic is implemented)
+// among the key components
+int COMPOSITE_KEY_security_bits(COMPOSITE_KEY * sec_bits);
+
+
+#ifdef  __cplusplus
+}
+#endif
+#endif
+
+/* END: composite_local.h */
