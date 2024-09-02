83a6edb7ad33d04348c81a67a3f0417fe76823a0
Dr. Pala <madwolf@openca.org>
2024-09-01 23:36:36 -0400

    Intermediate Save. Codebase refactoring.

---
+++ src/libpki/libconf/features.h
@@ -0,0 +1,22 @@
+/* src/libpki/libconf/features.h.  Generated from features.h.in by configure.  */
+/* features.h.in - enabled features definitions */
+
+#ifndef _LIBPKI_FEATURES_H
+#define _LIBPKI_FEATURES_H
+
+/* ECC Support for OpenSSL */
+#define ENABLE_ECDSA 1
+
+/* Open Quantum Safe Library */
+/* #undef ENABLE_OQS */
+
+/* Open Quantum Safe Library */
+/* #undef ENABLE_OQSPROV */
+
+/* Composite Crypto Native OpenSSL Support */
+/* #undef ENABLE_COMPOSITE */
+
+/* Combined/Alt Crypto Native OpenSSL Support */
+/* #undef ENABLE_COMBINED */
+
+#endif // End of _LIBPKI_FEATURES_H
